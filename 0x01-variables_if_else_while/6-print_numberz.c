221b09ee998574c4381249e3f8e905c1cd8d5d6c
altair1990e <altair1990e@gmail.com>
2022-10-14 18:52:46 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: one if there is an error
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
