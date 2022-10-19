11638c3be374c9737f577090a5e9cd08c52e6062
altair1990e <altair1990e@gmail.com>
2022-10-19 01:26:02 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet in lower case
+ * written by Altair
+ * Return:1
+ */
+
+void print_alphabet(void);
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+	return (0);
+}
