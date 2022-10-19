7158c678a03b2f6650de72fbf78dab969bf4963d
altair1990e <altair1990e@gmail.com>
2022-10-19 04:00:43 -0700

    modified task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,15 +17,15 @@ void jack_(void)
 			{
 				for (m = 0; m <= 9; m++);
 				{
-				if (i == 2 && j > 3)
-			{break;
-				}
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar (m + '0');
-					_putchar('\n');
+					if (i == 2 && j > 3)
+					{break;
+					}
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar (m + '0');
+						_putchar('\n');
 				}
 			
 			}
