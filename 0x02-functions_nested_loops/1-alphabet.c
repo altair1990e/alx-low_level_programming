b8ed413c3443f1dd7f9426ed5c5da75b73725cee
altair1990e <altair1990e@gmail.com>
2022-10-19 02:06:29 -0700

    task 1 all over again

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * main - print the alphabet in lower case
- * written by Altair
- * Return:1
+ * print_alphabet - print all alphabets in lowercase
+ * Return: 1
  */
-
 void print_alphabet(void)
 {
-	char i;	
-	
+	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
-	}	
-		_putchar('\n');
+	_putchar(i);
 	}
+	_putchar('\n')
 }
