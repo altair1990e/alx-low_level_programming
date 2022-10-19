72cd1117c47ad5572aa94bab708dc804a06bd3e2
altair1990e <altair1990e@gmail.com>
2022-10-19 14:15:28 -0700

    last task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 
 	n1 = 1;
 	n2 = 2;
-	fn = afn =0;
+	fn = afn = 0;
 	while (fn <= 4000000)
 	{
 		fn = n1 + n2;
@@ -24,6 +24,6 @@ int main(void)
 			afn += n1;
 		}
 	}
-	printf("%ld'n", afn);
+	printf("%ld\n", afn);
 	return (0);
 }
