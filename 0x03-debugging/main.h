bc019f5bd05df4dacc0f15209a384888c8be8090
altair1990e <altair1990e@gmail.com>
2022-10-20 12:37:00 -0700

    task 1

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include </alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
