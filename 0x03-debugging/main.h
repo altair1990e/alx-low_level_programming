a48f0b1734244135247a67f03283e6692e8dca77
altair1990e <altair1990e@gmail.com>
2022-10-20 13:47:12 -0700

    task 0 again

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include </alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c>
+#include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
