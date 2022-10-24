4a29be16e6f2fdb9f248d8ce6f7eb53c83434269
altair1990e <altair1990e@gmail.com>
2022-10-24 11:48:55 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * *n@: n pointer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project is about arrays, pointers and strings
