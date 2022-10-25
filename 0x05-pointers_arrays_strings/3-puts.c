eb15bcb7111b9d9afddb1bb4374650ea5c8fabeb
altair1990e <altair1990e@gmail.com>
2022-10-24 20:19:22 -0700

    5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,7 +9,7 @@
 void _puts(char *str)
 {
 	int count = 0;
-	
+
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
@@ -19,5 +19,4 @@ void _puts(char *str)
 		}
 		_putchar(str[count]);
 		count++;
-	}
 }
